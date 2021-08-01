a2e1be0063f97f81ee5b8ceb4b4a5cb5f0596e13
Lucas Rotsen <lucasrotsen@gmail.com>
2021-08-01 12:59:44 -0300

    Fixes QueuePool limit overflow

--- database/config.py
+++ database/config.py
@@ -6,7 +6,7 @@ from api.config import POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DATABASE
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@localhost/{POSTGRES_DATABASE}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=50, max_overflow=-1)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
